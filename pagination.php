071060943dd051548fb66413f4f123517a77480b
zulacom <luthfieabdulazis@gmail.com>
2016-05-03 02:19:07 +0700

    v1.0.0

---
+++ pagination.php
@@ -0,0 +1,21 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/* This Application Must Be Used With BootStrap 4 *  */
+$config['full_tag_open'] 	= '<div class="pagging text-center"><nav><ul class="pagination">';
+$config['full_tag_close'] 	= '</ul></nav></div>';
+$config['num_tag_open'] 	= '<li class="page-item"><span class="page-link">';
+$config['num_tag_close'] 	= '</span></li>';
+$config['cur_tag_open'] 	= '<li class="page-item active"><span class="page-link">';
+$config['cur_tag_close'] 	= '<span class="sr-only">(current)</span></span></li>';
+$config['next_tag_open'] 	= '<li class="page-item"><span class="page-link">';
+$config['next_tagl_close'] 	= '<span aria-hidden="true">&raquo;</span></span></li>';
+$config['prev_tag_open'] 	= '<li class="page-item"><span class="page-link">';
+$config['prev_tagl_close'] 	= '</span></li>';
+$config['first_tag_open'] 	= '<li class="page-item"><span class="page-link">';
+$config['first_tagl_close'] = '</span></li>';
+$config['last_tag_open'] 	= '<li class="page-item"><span class="page-link">';
+$config['last_tagl_close'] 	= '</span></li>';
+
+// end of file Pagination.php 
+// Location config/pagination.php 
+// @zulacom
